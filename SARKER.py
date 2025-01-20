ee57ecbd0588bf3cdc3203ef9509132374313e26
SEFAT SARKER <114663032+SEFAT-777@users.noreply.github.com>
2025-01-20 20:54:35 +0600

    Create SARKER.py

---
+++ SARKER.py
@@ -0,0 +1,7 @@
+import os, sys, platform
+bit = platform.architecture()[0]
+if bit == '64bit':
+    os.system(f"mv checker.cpython-312.so {sys.path[2]}/checker.cpython-312.so")
+    import SARKER
+else:
+    sys.exit("32 bit not work")
