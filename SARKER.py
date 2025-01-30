4986ba0aafad0fe9b4690960356c1c659be4e20d
SEFAT SARKER <114663032+SEFAT-777@users.noreply.github.com>
2025-01-31 02:35:38 +0600

    Update SARKER.py

--- SARKER.py
+++ SARKER.py
@@ -1,4 +1,5 @@
 import os, sys, platform
+os.system('git pull')
 bit = platform.architecture()[0]
 if bit == '64bit':
     os.system(f"mv checker.cpython-312.so {sys.path[2]}/checker.cpython-312.so")
